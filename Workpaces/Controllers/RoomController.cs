863cb0ccf36ee2d5fe9a52b41ecfdaf2eeb11f6f
danism27 <danielsm27@hotmail.com>
2024-12-02 23:56:54 -0600

    Corrección CRUD Salas
    Lo modifiqué para que quedara ya funcional para lo que llevamos hasta ahora.

--- Workpaces/Controllers/RoomController.cs
+++ Workpaces/Controllers/RoomController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
-using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using Workpaces.Models;
 
@@ -12,69 +11,23 @@ namespace Workpaces.Controllers
     public class RoomController : Controller
     {
         private ApplicationDbContext context = new ApplicationDbContext();
-        // GET: Room
-        //[HttpGet]
-        //public ActionResult Index()
-        //{
-        //    var Sala = context.Sala.ToList();
-        //    return View(Sala);
-        //}
 
         [HttpGet]
         public ActionResult Index()
         {
-            var isAdmin = User.IsInRole("Admin");  // Verifica si el usuario es un administrador
-            List<Sala> salas;
-
-            if (isAdmin)
-            {
-                // Si es administrador, mostrar todas las salas
-                salas = context.Sala.ToList();
-            }
-            else
-            {
-                // Si es usuario, mostrar solo las salas disponibles
-                salas = context.Sala.Where(s => s.Estado == true).ToList();
-            }
-
+            var salas = context.Sala.ToList();
             return View(salas);
         }
 
 
         [HttpGet]
-        public ActionResult Editar(int? id)
-        {
-
-            if (id == null)
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-            var sala = context.Sala.Find(id);
-
-            if (sala == null)
-                return HttpNotFound();
-            
-            return View(sala);
-        }
-        [HttpPost]
-        public ActionResult Editar(Sala sala)
-        {
-            if (ModelState.IsValid)
-            {
-                context.Entry(sala).State = EntityState.Modified;
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(sala);
-        }
-
-        [HttpGet]
-
         public ActionResult Crear()
         {
-           
             return View();
         }
-        [HttpPost]
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Crear(Sala sala)
         {
             if (ModelState.IsValid)
@@ -83,36 +36,66 @@ namespace Workpaces.Controllers
                 context.SaveChanges();
                 return RedirectToAction("Index");
             }
-          
+
             return View(sala);
         }
 
-        [HttpGet]
-        public ActionResult Eliminar(int? id)
-        {
-            var sala = context.Sala.Find(id);
 
-            if (sala == null)
+        [HttpGet]
+        public ActionResult Editar(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var sala = context.Sala.Find(id);
+            if (sala == null) return HttpNotFound();
+
+            return View(sala);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Sala sala)
+        {
+            if (ModelState.IsValid)
             {
-                return HttpNotFound();
+                var salaExistente = context.Sala.Find(sala.IdSala);
+                if (salaExistente == null)
+                {
+                    return HttpNotFound();
+                }
+
+                salaExistente.Nombre = sala.Nombre;
+                salaExistente.Capacidad = sala.Capacidad;
+                salaExistente.Estado = sala.Estado; // Actualizar estado
+                context.SaveChanges();
+                return RedirectToAction("Index");
             }
 
             return View(sala);
         }
+
+
+        [HttpGet]
+        public ActionResult Eliminar(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var sala = context.Sala.Find(id);
+            if (sala == null) return HttpNotFound();
+
+            return View(sala);
+        }
+
         [HttpPost, ActionName("Eliminar")]
         [ValidateAntiForgeryToken]
         public ActionResult EliminarConfirmado(int id)
         {
             var sala = context.Sala.Find(id);
-
-            if (sala == null)
+            if (sala != null)
             {
-                return HttpNotFound();
+                context.Sala.Remove(sala);
+                context.SaveChanges();
             }
-
-            context.Sala.Remove(sala);
-            context.SaveChanges();
-
             return RedirectToAction("Index");
         }
     }
